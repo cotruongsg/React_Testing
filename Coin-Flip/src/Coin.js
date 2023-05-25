fb3eb6d6b39aa01328cfb55e1d07e9ef046379ef
Truong Duong <cotruongsg@gmail.com>
2023-05-25 00:53:45 -0700

    Add Coin Flip demo

---
+++ Coin-Flip/src/Coin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Coin.css";
+
+function Coin(props) {
+  return (
+    <div className="Coin">
+      <img src={props.imgSrc} alt={props.side}></img>
+    </div>
+  );
+}
+
+export default Coin;
---
+++ Coin-Flip/src/CoinContainer.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Coin from "./Coin";
+import "./CoinContainer.css";
+import coin_head from "./coin-head.jpg";
+import coin_tail from "./coin-tail.jpg";
+import { choice } from "./helpers";
+
+function CoinContainer(props) {
+  const [coin, setCoin] = useState(null);
+  const [headCount, setHeadCount] = useState(0);
+  const [tailCount, setTailCount] = useState(0);
+
+  const flipcoin = () => {
+    const newCoin = choice(props.coins);
+    setCoin(newCoin);
+    if (newCoin.side === "head") {
+      setHeadCount((headCount) => headCount + 1);
+    } else {
+      setTailCount((tailCount) => tailCount + 1);
+    }
+  };
+
+  const currCoin = coin ? <Coin side={coin.side} imgSrc={coin.imgSrc} /> : null;
+
+  return (
+    <div className="CoinContainer">
+      <h2>Let's flip a coin</h2>
+      {currCoin}
+      <button onClick={flipcoin}>Flip me!</button>
+      <p>
+        Out of {headCount + tailCount} flips , there have been {headCount} heads
+        and {tailCount} tails.
+      </p>
+    </div>
+  );
+}
+
+CoinContainer.defaultProps = {
+  coins: [
+    {
+      side: "head",
+      imgSrc: coin_head,
+    },
+    {
+      side: "tail",
+      imgSrc: coin_tail,
+    },
+  ],
+};
+
+export default CoinContainer;
